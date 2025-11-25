cb9083ec019c4355f5d3feaa47fe3b7e73705e90
zitaharry <harrymuigua@gmail.com>
2025-11-25 13:53:28 +0300

    Implement Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Navbar from "@components/Navbar";
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 };
 
 export default App;
